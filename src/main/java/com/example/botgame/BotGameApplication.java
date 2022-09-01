c8be137332c182e03b0d6945a5b5d25beee8edbd
Lidiia <lidochkapop@outlook.com>
2022-09-01 12:01:00 +0300

    Initial commit

---
+++ src/main/java/com/example/botgame/BotGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.botgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/botgame/BotGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
